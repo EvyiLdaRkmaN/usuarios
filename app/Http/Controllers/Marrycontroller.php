0330a616b9e43ffeacc7809ea250f35fa936f5d9
Richard Yordy <evil.darkman@hotmail.com>
2022-09-28 13:48:44 -0500

    clean marryController

--- app/Http/Controllers/Marrycontroller.php
+++ app/Http/Controllers/Marrycontroller.php
@@ -16,55 +16,5 @@ class MarryController extends Controller
         
         return view('marry.index', compact('marrys'));
     }
-
-    public function create(){
-        return view('usuarios.create');
-    }
-    
-    public function marry(){
-        $usuarios = Usuario::orderBY('id', 'desc')
-            ->where('sexo', 'Masculino')
-            ->where('estado', 'Soltero')
-            ->paginate();
-        $usuariosF = Usuario::orderBY('id', 'desc')
-            ->where('sexo', 'Femenino')
-            ->where('estado', 'Soltero')
-            ->paginate();
-        return view('usuarios.marry', compact('usuarios'), compact('usuariosF'));
-    }
-
-    public function store(StoreUsuario $request){
-
-
-        $usuario = Usuario::create($request->all());
-
-        return redirect()->route('usuarios.index', $usuario);
-    }
-
-    public function show(Usuario $usuario){
-        return view('usuarios.show', compact('usuario'));
-    }
-
-    public function edit(Usuario $usuario){
-        return view('usuarios.edit', compact('usuario'));
-    }
-
-    public function update(Request $request, Usuario $usuario){
-
-        $request->validate([
-            'nombre' => 'required',
-            'edad' => 'required',
-            'sexo'=> 'required',
-        ]);
-        
-        $usuario->update($request->all());
-        return redirect()->route('usuarios.show', $usuario);
-    }
-
-    public function destroy(Usuario $usuario){
-        $usuario->delete();
-        return redirect()->route('usuarios.index');
-    }
-
     
 }
